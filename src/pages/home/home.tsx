972736caeeb0964c2000a5a501ca08053f47cd1f
himenoworks <himeno.works001@gmail.com>
2022-08-31 13:58:49 +0700

    create input url

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,28 @@
 import React from "react";
+import "./home.scss";
 
 const HomePage: React.FC = () => {
-    return <></>;
-}
+	return (
+		<>
+			<div className="home-page">
+				<div className="section-1">
+					<div className="card">
+						<div className="card-header">
+							<h1>Place your url here!</h1>
+							<div className="replace-url">
+								<div className="url-input">
+									<input type="text" placeholder="Replace you url here" />
+								</div>
+								<div className="button-input">
+									<button className="btn btn-primary">Generate</button>
+								</div>
+							</div>
+						</div>
+						<div className="card-body"></div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
 export default HomePage;
